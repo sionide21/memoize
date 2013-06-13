0c9b5bf5dfc0c158578581bd3f6498745bc7d41b
Ben Olive <sionide21@gmail.com>
2013-06-13 18:36:00 -0400

    Add setup file and license

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='memoize',
+    version='1.0.0',
+    author='Ben Olive',
+    author_email='sionide21@gmail.com',
+    description='''
+        Create properties that are only computed the first time they are called
+    ''',
+    url='https://github.com/sionide21/memoize',
+
+    py_modules=['memoize'],
+    platforms='all',
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
